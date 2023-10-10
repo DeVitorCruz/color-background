913cd87d5e9986a8c8515d11c463650a8e23389e
DeVitor <97829726+DeVitorCruz@users.noreply.github.com>
2023-10-10 12:12:40 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,35 +1,102 @@
 "use strict";
 const listColor = [
-    "--blue",
-    "--indigo",
-    "--purple",
-    "--pink",
-    "--red",
-    "--orange",
-    "--yellow",
-    "--green",
-    "--teal",
-    "--cyan",
-    "--white",
-    "--gray",
-    "--gray-dark",
-    "--primary",
-    "--secondary",
-    "--success",
-    "--info",
-    "--warning",
-    "--danger",
-    "--light",
-    "--dark"
+    {
+        name: "--white",
+        color: "#fff"
+    },
+    {
+        name: "--blue",
+        color: "#007bff"
+    },
+    {
+        name: "--indigo",
+        color: "#6610f2",
+    },
+    {
+        name: "--purple",
+        color: "#6f42c1"
+    },
+    {
+        name: "--pink",
+        color: "#e83e8c"
+    },
+    {
+        name: "--red",
+        color: "#dc3545",
+    },
+    {
+        name: "--orange",
+        color: "#fd7e14"
+    },
+    {
+        name: "--yellow",
+        color: "#ffc107"
+    },
+    {
+        name: "--green",
+        color: "#28c745"
+    },
+    {
+        name: "--teal",
+        color: "#20c997"
+    },
+    {
+        name: "--cyan",
+        color: "#17a2b8"
+    },
+    {
+        name: "--gray",
+        color: "#6c757d"
+    },
+    {
+        name: "--gray-dark",
+        color: "#343a40"
+    },
+    {
+        name: "--primary",
+        color: "#007bff"
+    },
+    {
+        name: "--secondary",
+        color: "#6c757d"
+    },
+    {
+        name: "--success",
+        color: "#28a745"
+    },
+    {
+        name: "--info",
+        color: "#17a2b8"
+    },
+    {
+        name: "--warning",
+        color: "#ffc107"
+    },
+    {
+        name: "--danger",
+        color: "#dc3545",
+    },
+    {
+        name: "--light",
+        color: "#f8f9fa"
+    },
+    {
+        name: "--dark",
+        color: "#343a40"
+    }
 ];
 let randomIndex = Math.floor(Math.random() * listColor.length);
 $(document).ready(function () {
+    $(window).on("load", function () {
+        $("#hex-value").text(`${listColor[0].color}`);
+    });
     $("#btn").click(function () {
         let currentIndex = Math.floor(Math.random() * listColor.length);
         while (randomIndex == currentIndex) {
             currentIndex = Math.floor(Math.random() * listColor.length);
         }
-        $("body").css("background-color", `var(${listColor[currentIndex]})`);
+        $("#hex-value").text(`${listColor[currentIndex].color}`);
+        $("body").css("background-color", `var(${listColor[currentIndex].name})`);
         randomIndex = currentIndex;
     });
 });
